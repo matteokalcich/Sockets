54e3054d0fac33b9cf06891c0246990a01585086
matteokalcich <matteokalcich@gmail.com>
2024-09-21 17:52:26 +0200

    new stream input output

--- src/Client.java
+++ src/Client.java
@@ -1,26 +1,26 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
 
     Socket s = null;
-    DataInputStream din = null;
-    DataOutputStream dout = null;
+    BufferedReader din = null;
+    PrintWriter dout = null;
     Scanner sc = new Scanner(System.in);
     private boolean bool_listenSrv = true;
-
     private boolean bool_inptCapt = true;
 
     public Client() {
         try {
-            s = new Socket("192.168.1.4", 3333);
+            s = new Socket("192.168.1.2", 3333);
 
-            din = new DataInputStream(s.getInputStream());
-            dout = new DataOutputStream(s.getOutputStream());
+            din = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            dout = new PrintWriter(new OutputStreamWriter(s.getOutputStream()), true);
 
             // Start a separate thread to listen for server messages
             new Thread(this::listenForServerMessages).start();
@@ -28,16 +28,6 @@ public class Client {
             // Start a thread to capture the input until the user writes "stop"
             new Thread(this::captureInput).start();
 
-            // System.out.println("Quante volte vuoi scrivere?");
-            // System.out.print("> ");
-            // int volte = Integer.parseInt(sc.nextLine());
-
-            // for (int i = 0; i < volte; i++) {
-            // richiesta();
-            // }
-
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -46,58 +36,44 @@ public class Client {
     private void listenForServerMessages() {
         while (bool_listenSrv) {
             try {
-                String input = din.readUTF();
+                String input = din.readLine();
 
-                // System.out.println(input);
-                if (input.equals("stop")) {
-
+                if ("stop".equalsIgnoreCase(input)) {
                     System.out.println(input);
-
                     closeConnection();
-
                     System.exit(0);
-
+                } else {
+                    System.out.println("Risposta del server: " + input);
                 }
             } catch (IOException e) {
                 if (bool_listenSrv) {
                     e.printStackTrace();
                 }
-                // If not running, the socket has been closed intentionally
             }
         }
     }
 
     private void captureInput() {
-
         System.out.println("Inizia a scrivere");
-
         while (bool_inptCapt) {
-
             try {
-
                 System.out.print("\n - ");
-
                 String input = sc.nextLine();
-
-                dout.writeUTF(input);
-
+                dout.println(input); // Invia il messaggio al server
+                if ("chiuso".equalsIgnoreCase(input)) {
+                    bool_listenSrv = false;
+                }
             } catch (Exception e) {
-
                 e.printStackTrace();
-
             }
         }
-
     }
 
     private void closeConnection() {
         try {
             bool_listenSrv = false;
-
             din.close();
-
             dout.close();
-
             s.close();
         } catch (IOException e) {
             e.printStackTrace();
