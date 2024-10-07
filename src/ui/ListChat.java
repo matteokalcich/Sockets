1083a4871148dab64feed94d33f17fab2029e02e
matteokalcich <matteokalcich@gmail.com>
2024-10-07 09:30:09 +0200

    added check for list chat and client selected

--- src/ui/ListChat.java
+++ src/ui/ListChat.java
@@ -7,44 +7,40 @@ import javax.swing.JPanel;
 
 import java.awt.event.*;
 
-public class ListChat extends JPanel{
-	
+public class ListChat extends JPanel {
+
 	private JList<String> chat_clients;
-	
+
 	public ListChat(DefaultListModel<String> tmp) {
-		
+
 		this.chat_clients = new JList<String>(tmp);
 		this.add(chat_clients);
 		this.chat_clients.setBounds(0, 0, 50, 50);
 
 		this.chat_clients.addMouseListener(new MouseAdapter() {
-         public void mouseClicked(MouseEvent me) {
-            if (me.getClickCount() == 1) {
-               JList target = (JList)me.getSource();
-               int index = target.locationToIndex(me.getPoint());
-               if (index >= 0) {
-                  Object item = target.getModel().getElementAt(index);
-                  JOptionPane.showMessageDialog(null, item.toString());
-               }
-            }
-         }
-      });
+			public void mouseClicked(MouseEvent me) {
+				if (me.getClickCount() == 1) {
+					JList target = (JList) me.getSource();
+					int index = target.locationToIndex(me.getPoint());
+					if (index >= 0) {
+						Object item = target.getModel().getElementAt(index);
+						JOptionPane.showMessageDialog(null, item.toString());
 
+						for (int i = 0; i < tmp.size(); i++){
 
-		try {
-
-
-			System.out.println("Hai scelto: " + this.chat_clients.getSelectedValue().toString());
-			
-		} catch (Exception e) {
-
-		}
-		
+							if (item == tmp.get(i)) {
+								
+								JOptionPane.showMessageDialog(null, "I client coincidono");
+							}
+						}
+						
+					}
+				}
+			}
+		});
 
 		this.revalidate();
 		this.repaint();
-
-		//TODO mettere il metodo per un eventlistener a ogni socket nìin chat_clients
 	}
 
 }
