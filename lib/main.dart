f1f9de118d191a763ea0987ec78ef808ca338203
matteokalcich <matteokalcich@gmail.com>
2024-10-10 20:10:02 +0200

    startup gui ListChat

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutterclient/screens/ChosenChat.dart';
+import 'package:flutterclient/screens/ListChat.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ChosenChat(),
+      home: ListChat(),
     );
   }
 }
