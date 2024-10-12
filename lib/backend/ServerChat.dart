76be15e222848b54c47a41c00a6ae51af52151e5
matteokalcich <matteokalcich@gmail.com>
2024-10-12 14:59:10 +0200

    minor changes

--- lib/backend/ServerChat.dart
+++ lib/backend/ServerChat.dart
@@ -5,54 +5,50 @@ import 'MessageReceiver.dart';
 
 
 class ServerChat {
-  Socket? socket;
+  late Socket socket;
 
   // Metodo per connettersi al server
   Future<void> connectToServer() async {
     try {
-      socket = await Socket.connect('192.168.1.10', 3333);
+      socket = await Socket.connect('192.168.1.9', 3333);
       print(
-          'Connesso al server con l\'IP: ${socket!.remoteAddress.address} e porta: ${socket!.remotePort}');
+          'Connesso al server con l\'IP: ${socket.remoteAddress.address} e porta: ${socket.remotePort}');
     } catch (e) {
       print('Errore di connessione: $e');
-      socket = null; // Impostiamo su null per evitare problemi successivi
+
     }
   }
 
   // Metodo per chiudere la connessione
   void closeConnection() {
-    socket?.write('exit');
-    socket?.close();
+    socket.write('exit');
+    socket.close();
     print('Connessione chiusa');
   }
 
   // Metodo per inviare un messaggio
   Future<void> sendMessage(String message) async {
-    if (socket != null) {
-      socket!.writeln(message);
-      await socket!.flush();
-    } else {
-      print("Errore: socket non connesso");
-    }
+    
+    socket.writeln(message);
+    await socket.flush();
+    
   }
 
   // Metodo per iniziare a ricevere messaggi dal server
   void startReceivingMessages() {
-    if (socket != null) {
-      print('Avviando ascolto dei messaggi dal server...');
-      socket!.listen((data) {
-        String message = String.fromCharCodes(data).trim();
-        MessageReceiver.instance.sendPort
-            .send(message); // Invia il messaggio all'Isolate
-        print("Messaggio ricevuto dal server: $message");
-      }, onDone: () {
-        print('Socket chiuso dal server');
-      }, onError: (error) {
-        print('Errore nel socket: $error');
-      });
-    } else {
-      print(
-          'Errore: socket non connesso, impossibile iniziare a ricevere messaggi');
-    }
+    
+    print('Avviando ascolto dei messaggi dal server...');
+    socket.listen((data) {
+
+      String message = String.fromCharCodes(data).trim();
+      MessageReceiver.instance.sendPort
+        .send(message); // Invia il messaggio all'Isolate
+      print("Messaggio ricevuto dal server: $message");
+    }, onDone: () {
+      print('Socket chiuso dal server');
+    }, onError: (error) {
+      print('Errore nel socket: $error');
+    });
+    
   }
 }
