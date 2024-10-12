54f313ecc4e7e876d4c73397590a920d20284042
matteokalcich <matteokalcich@gmail.com>
2024-10-12 14:59:50 +0200

    added parameters

--- lib/screens/ChosenChat.dart
+++ lib/screens/ChosenChat.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutterclient/screens/ListChat.dart';
 
 import '../backend/MessageReceiver.dart';
 import '../backend/ServerChat.dart';
 
 class ChosenChat extends StatefulWidget {
-
   final String name_clients; // Aggiungi il parametro a ChosenChat
+  final ServerChat? serverChat;
 
   // Costruttore per il widget ChosenChat
-  ChosenChat({required this.name_clients, Key? key}) : super(key: key);
+  ChosenChat({required this.name_clients, required this.serverChat, Key? key})
+      : super(key: key);
 
   @override
   _ChosenChat createState() => _ChosenChat();
@@ -18,25 +20,46 @@ class ChosenChat extends StatefulWidget {
 
 class _ChosenChat extends State<ChosenChat> {
 
-  late String name_clients;
-
-  //late ServerChat serverChat; // Istanza del server chat
   final List<String> messages = []; // Lista dei messaggi
   final TextEditingController _controller =
       TextEditingController(); // Controller per la TextField
-  final ScrollController _scrollController =
-      ScrollController(); // Controller per il ListView
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
+    creaListener();
     //serverChat = ServerChat(); // Inizializzo ServerChat
     //waitServerConnection(); // Avvio la connessione al server
+  }
+
+  Future<void> creaListener() async {
+    await listener();
+  }
+
+  Future<void> listener() async {
+    // Avvio l'Isolate per l'ascolto
+    await MessageReceiver.instance.startListening();
+
+    
+    // Ascolto i messaggi ricevuti dall'Isolate
+    MessageReceiver.instance.broadcastStream.listen((message) {
+      setState(() {
+        messages.add(message); // Aggiungi il messaggio alla lista
+        _scrollToBottom(); // Scorri automaticamente verso il basso
+      });
+      print("Messaggio ricevuto dall'Isolate: $message");
+    });
+
+    // Quando sei pronto per iniziare a ricevere messaggi dal server
+    //widget.serverChat?.startReceivingMessages();
   }
   /*
   Future<void> waitServerConnection() async {
     await initServerConnection();
   }
+
+  
 
   // Inizializza la connessione al server e l'ascolto dei messaggi
   Future<void> initServerConnection() async {
@@ -82,15 +105,13 @@ class _ChosenChat extends State<ChosenChat> {
   }
 
   // Invia un messaggio al server
- /* void _sendMessage() {
+  void _sendMessage() {
     final text = _controller.text.trim();
     if (text.isNotEmpty) {
-      serverChat.sendMessage(text); // Invia il messaggio
+      widget.serverChat?.sendMessage(text); // Invia il messaggio
       _controller.clear(); // Pulisci il TextField
     }
   }
-
-  */
 
   @override
   Widget build(BuildContext context) {
@@ -111,7 +132,6 @@ class _ChosenChat extends State<ChosenChat> {
                   controller: _scrollController,
                   itemBuilder: (context, index) {
                     return Container(
-                      
                       color: Colors.black,
                       child: IntrinsicWidth(
                         child: ListTile(
@@ -141,12 +161,11 @@ class _ChosenChat extends State<ChosenChat> {
                     ),
                   ),
                 ),
-                /*IconButton(
+                IconButton(
                   icon: Icon(Icons.send),
                   onPressed:
-                    
                       _sendMessage, // Chiama la funzione per inviare messaggi
-                ),*/
+                ),
               ],
             ),
           ),
